e6876fe96ce04c7e599734986ff9a0998fe72e85
CESAR URDANETA <115049155+hidrolisis@users.noreply.github.com>
2023-03-15 00:56:13 -0300

    Delete src directory

--- src/components/CartWidget/index.jsx
+++
@@ -1,8 +0,0 @@
-export const CartWidget = () => {
-  return (
-    <>
-      
-      <div>5</div>
-    </>
-  );
-};
--- src/components/ItemListContainer/index.jsx
+++
@@ -1,3 +0,0 @@
-export const ItemListContainer = (props) => {
-  return <div className="itemListContainerDisplay">{props.text}</div>;
-};
